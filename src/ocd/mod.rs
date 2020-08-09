df3b24bbc28e19cad2261c8c4e30a6ffd0b76746
Iñaki Garay <igarai@gmail.com>
2020-08-09 11:37:42 -0300

    Docs

--- src/ocd/mod.rs
+++ src/ocd/mod.rs
@@ -5,6 +5,8 @@ pub mod tss;
 use crate::ocd::mrn::MassRenameConfig;
 use crate::ocd::tss::TimeStampSortConfig;
 
+/// The Command enum represents which subcommand ocd will run and carries its
+/// configuration with it.
 #[remain::sorted]
 #[derive(Clone, Debug)]
 pub enum Command {
