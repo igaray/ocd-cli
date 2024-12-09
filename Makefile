0e46a38a81c5fd33d72692f63e83ae1cec62644b
Iñaki Garay <igarai@gmail.com>
2024-12-09 10:29:00 -0300

    Changed version string to include package version and git hash

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
-.PHONY: usage build install uninstall
+.DEFAULT_GOAL = usage
+default: usage
 
+.PHONY: usage
 usage:
 	@echo "usage:"
 	@echo "    build:     build the command"
 	@echo "    install:   installs the command binary executable into $HOME/.local/bin/"
 	@echo "    uninstall: removes the command binary executable from $HOME/.local/bin/s"
 
+.PHONY: build
 build:
 	cargo build --release
 
-install: build uninstall
+.PHONY: install
+install: uninstall build
 	@cp target/release/ocd ${HOME}/.local/bin/
 
+.PHONY: uninstall
 uninstall:
 	@rm -f ${HOME}/.local/bin/ocd
--- build.rs
+++ build.rs
@@ -1,18 +1,30 @@
+extern crate clap;
 extern crate lalrpop;
 
+use clap::crate_version;
 use std::process::Command;
 
 fn main() {
-    lalrpop::Configuration::new()
+    // Set up LALRPOP.
+    let lalrpop_config = lalrpop::Configuration::new()
         .log_verbose()
-        .process_current_dir()
-        .unwrap();
+        .process_current_dir();
+    match lalrpop_config {
+        Ok(x) => {
+            println!("LALRPOP build ran correctly:\n{:?}", x);
+        }
+        Err(e) => {
+            eprintln!("Error while running LALRPOP during build: {:?}", e);
+        }
+    }
 
-    // taken from https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
+    // Get the current git commit hash.
+    // Taken from https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
     let output = Command::new("git")
         .args(["rev-parse", "HEAD"])
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    let version_string = format!("{}-{}", crate_version!(), git_hash);
+    println!("cargo:rustc-env=VERSION_STR={}", version_string);
 }
